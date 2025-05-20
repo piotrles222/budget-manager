ac8aae5995ad170e8e4d3b46e7137ee057ec981e
piotrles222 <piotrek.lesniczak@gmail.com>
2025-05-20 21:45:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from models.models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/budget.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/')
 def home():
     return "Hello, Budget Manager!"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
