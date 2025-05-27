d46c5fc1d6aeb817d89b4306e736d44c395f262e
piotrles222 <piotrek.lesniczak@gmail.com>
2025-05-27 11:12:03 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,1 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+class TransactionForm(FlaskForm):
+    amount = FloatField("Kwota", validators=[DataRequired()])
+    description = StringField("Opis")
+    is_income = BooleanField("Dochód?")
+    category = SelectField("Kategoria", coerce=int)
+    submit = SubmitField("Dodaj transakcję")
 
